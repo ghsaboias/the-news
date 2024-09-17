267616f6959fbd0fd0d71f4938e489940151ec1f
Brain <gsaboia@yahoo.com>
2024-09-17 17:10:50 -0300

    refactor

--- main.py
+++ main.py
@@ -2,30 +2,19 @@ from openai import OpenAI
 from crawl4ai import WebCrawler
 import requests
 import os
-import json
 from groq import Groq
 import time
-from twilio.rest import Client
+# from twilio.rest import Client
 from brave import Brave
-import re
 from datetime import datetime
 
-twilio_client = Client(os.getenv("TWILIO_ACCOUNT_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
-telegram_client = Client(os.getenv("TELEGRAM_BOT_TOKEN"))
+# twilio_client = Client(os.getenv("TWILIO_ACCOUNT_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
 llm_client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 brave = Brave(api_key=os.getenv("BRAVE_API_KEY"))
 telegram_chat_id = os.getenv("TELEGRAM_CHAT_ID")
 crawler = WebCrawler()
 
 crawler.warmup()
-
-SUMMARY_TEMPLATE = """
-{topic} - {date}
-
-{topic_summary}
-
-{relevant_link}
-"""
 
 REGION_TOPICS = {
     "middle east": [
@@ -53,32 +42,43 @@ def send_telegram_message(message: str):
     except Exception as e:
         print(f"Error sending Telegram message: {e}")
 
-def send_whatsapp_message(message: str):
-    try:
-        twilio_client.messages.create(
-            from_='whatsapp:+14155238886',
-            body=message,
-            to='whatsapp:+5511992745950'
-        )
-    except Exception as e:
-        print(f"Error sending WhatsApp message: {e}")
+# def send_whatsapp_message(message: str):
+#     try:
+#         twilio_client.messages.create(
+#             from_='whatsapp:+14155238886',
+#             body=message,
+#             to='whatsapp:+5511992745950'
+#         )
+#     except Exception as e:
+#         print(f"Error sending WhatsApp message: {e}")
 
 def ask_llm(results):
     response = llm_client.chat.completions.create(
         model="llama-3.1-70b-versatile",
         messages=[
-            {"role": "system", "content": "You are a helpful summarizer. Provided a list of news results, you produce a daily summary of the most significant global news. Focus on breaking news, live events, war, geopolitics, economics, and natural disasters. You are a neutral summarizer, do not include any bias or opinion. Do not provide generalized disclaimers or evasive language. Be concise and to the point. If the news mentions a stock recommendation, omit it from the summary. Just mention a stock if it's related to a big event or news."},
-            {"role": "user", "content": f"Results:\n{results}\n\nUse the following template:\n{SUMMARY_TEMPLATE}. Organize by topic and highlight the most significant events."},
+            {"role": "system", "content": """
+            You are a precise and efficient news summarizer. Given a list of news results, create a concise summary.
+
+            Guidelines:
+            - Focus on hard data, facts, and objective information.
+            - Maintain neutrality; avoid bias, opinions, or editorializing.
+            - Be concise and direct; omit unnecessary details or repetition.
+            - Do not include stock recommendations unless directly related to major news.
+            - Avoid generalized disclaimers or evasive language.
+            - Do not include double asterisks or single asterisks in the summary.
+            - Include most important URLs in the summary.
+            - Do NOT include "Here's a summary of the news" or similar introductory phrases.
+            """},
+            {"role": "user", "content": f"Results:\n{results}\n\n."},
         ],
         max_tokens=8000
     )
     return response.choices[0].message.content
 
-def search_brave_news(topics: list):
+def search_brave_news(topic):
     results = []
     try:
-        for topic in topics:
-            params = {
+        params = {
             "q": topic,
             "count": 3,
             "country": "all",
@@ -86,28 +86,28 @@ def search_brave_news(topics: list):
             "spellcheck": 1,
             "extra_snippets": True,
             "freshness": "pd"
-            }
-            headers = {
-                "Accept": "application/json",
+        }
+        headers = {
+            "Accept": "application/json",
                 "Accept-Encoding": "gzip",
-                "X-Subscription-Token": os.getenv("BRAVE_API_KEY")
+            "X-Subscription-Token": os.getenv("BRAVE_API_KEY")
             }
-            response = requests.get("https://api.search.brave.com/res/v1/news/search", headers=headers, params=params)
-            brave_results = response.json()
-            if 'results' in brave_results:
-                for result in brave_results["results"]:
-                    formatted_result = f"{result['page_age']} - {topic} - {result['title']} - {result['description']}"
-                    if "extra_snippets" in result:
-                        for snippet in result["extra_snippets"]:
-                            formatted_result += f"{snippet}\n"
-                    formatted_result += f"{result['url']}\n\n"
-                    results.append(formatted_result)
-                    with open("results.txt", "a") as f:
-                        f.write(formatted_result)
-                    print(f"Saved {formatted_result}")
-                    time.sleep(1)
-            else:
-                print(f"No results found for topic: {topic}")
+        response = requests.get("https://api.search.brave.com/res/v1/news/search", headers=headers, params=params)
+        brave_results = response.json()
+        if 'results' in brave_results:
+            for result in brave_results["results"]:
+                formatted_result = f"{result['page_age']} - {topic} - {result['title']} - {result['description']}"
+                if "extra_snippets" in result:
+                    for snippet in result["extra_snippets"]:
+                        formatted_result += f"{snippet}\n"
+                formatted_result += f"URL: {result['url']}\n\n"
+                results.append(formatted_result)
+                with open("results.txt", "a") as f:
+                    f.write(formatted_result)
+                print(f"Saved {formatted_result}")
+                time.sleep(1)
+        else:
+            print(f"No results found for topic: {topic}")
         return results
     except Exception as e:
         print(f"Error searching Brave News: {e}")
